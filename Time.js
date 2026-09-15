3621659375870495c52744a6b63d8b78c1794460
lina-3-rgb <polinkavorobeva3@gmail.com>
2026-09-15 18:59:55 +0300

    Initial commit

---
+++ Time.js
@@ -0,0 +1,36 @@
+let a = 0;
+
+let timeElement;
+
+let txt = document.querySelector('.clic-txt');
+
+let btnStart = document.querySelector('.clic-stop');
+
+let btnPause = document.querySelector('.clic-paus');
+
+let btnReset = document.querySelector('.clic-reboot');
+
+let btnInc = document.querySelector('.clic-increase');
+
+let btnDec = document.querySelector('.clic-decrease');
+
+
+let start = () => {
+    timeElement = setInterval(() => {
+        a++;
+        txt.value = a;
+    }, 1000)
+}
+
+
+btnStart.addEventListener('click', Name)
+
+btnPause.addEventListener('click', () => {
+    clearInterval(timeElement);
+})
+
+btnReset.addEventListener('click', () => {
+    clearInterval(timeElement);
+    a = 0;
+    txt.value = a;
+})
